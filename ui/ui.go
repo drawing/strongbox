490bb1a1f55144073747e513d5901b0ce31d3391
drawing <cppbreak@qq.com>
2023-08-12 11:33:45 +0800

    update ui

--- ui/ui.go
+++ ui/ui.go
@@ -1,14 +1,15 @@
 package ui
 
 import (
-	"errors"
 	"fmt"
 	"log"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/driver/desktop"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -27,7 +28,7 @@ func ShowPasswordDialog(passChan chan string, win fyne.Window) {
 	passwdEntry := widget.NewPasswordEntry()
 	passwdEntry.Validator = func(input string) error {
 		if len(input) < leastPasswdLen {
-			return errors.New(fmt.Sprintf("must input least %d char", leastPasswdLen))
+			return fmt.Errorf("must input least %d char", leastPasswdLen)
 		}
 		passwd = input
 		return nil
@@ -65,7 +66,75 @@ func RunStrongBoxApp() {
 		desk.SetSystemTrayMenu(m)
 	}
 
-	win.SetContent(widget.NewLabel("Fyne System Tray"))
+	mountPointInput := widget.NewEntry()
+	mountPointInput.SetPlaceHolder("Enter MountPoint...")
+	// mountPointInput.Resize(fyne.NewSize(500, 50))
+
+	mountPointSelect := widget.NewButton("...", func() {
+		log.Println("Content was")
+		dlg := dialog.NewFolderOpen(func(dir fyne.ListableURI, err error) {
+			if err == nil && dir != nil {
+				log.Println("select:", dir.Path())
+			}
+		}, win)
+		dlg.Show()
+	})
+
+	mountRow := container.New(layout.NewGridLayout(2), mountPointInput, mountPointSelect)
+
+	backupInput := widget.NewEntry()
+	backupInput.SetPlaceHolder("Enter Backup Path...")
+	// mountPointInput.Resize(fyne.NewSize(500, 50))
+
+	backupSelect := widget.NewButton("...", func() {
+		log.Println("Content was")
+		dlg := dialog.NewFolderOpen(func(dir fyne.ListableURI, err error) {
+			if err == nil && dir != nil {
+				log.Println("select:", dir.Path())
+			}
+		}, win)
+		dlg.Show()
+	})
+
+	backupRow := container.New(layout.NewGridLayout(2), backupInput, backupSelect)
+
+	whitelist := widget.NewButton("Show Process Whitelist", func() {
+		log.Println("tapped")
+	})
+
+	forbidlist := widget.NewButton("Show Forbid Process", func() {
+		log.Println("tapped")
+	})
+
+	form := &widget.Form{
+		Items: []*widget.FormItem{ // we can specify items in the constructor
+			{Text: "Mount Point", Widget: mountRow},
+			{Text: "Backup", Widget: backupRow},
+			{Text: "Whitelist", Widget: whitelist},
+			{Text: "Forbidlist", Widget: forbidlist},
+		},
+		OnSubmit: func() { // optional, handle form submission
+			log.Println("Form submitted")
+			log.Println("multiline")
+		},
+		/*
+			OnCancel: func() { // optional, handle form submission
+				log.Println("Form submitted")
+				log.Println("multiline")
+			},
+		*/
+	}
+	form.CancelText = "Save Config"
+	form.SubmitText = "Mount"
+
+	// form.Append("Mount Point", mountRow)
+	// form.Append("Whitelist", whitelist)
+
+	content := container.NewVBox(
+		form,
+	)
+
+	win.SetContent(content)
 	win.SetCloseIntercept(func() {
 		win.Hide()
 	})
@@ -77,7 +146,7 @@ func RunStrongBoxApp() {
 	go func() {
 		select {
 		case passwd := <-passChan:
-			log.Println("recv pass ", passwd)
+			// log.Println("recv pass ", passwd)
 			if len(passwd) == 0 {
 				ShowQuicDialog(a, "must input passwd", win)
 			}
